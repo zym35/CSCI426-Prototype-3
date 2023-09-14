f6cb264fd12cceae477fd4eb1c0e2e13ac4257d4
King Zhou <zhouyimingking@outlook.com>
2023-09-14 02:08:18 -0700

    Add basic piranha behavior

--- Assets/Script/Piranha.cs
+++ Assets/Script/Piranha.cs
@@ -1,21 +1,80 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Piranha : MonoBehaviour
 {
-    public List<Piranha> s;
-    
+    public Transform tongue;
+    public LineRenderer tongueLineRenderer;
+    public float detectDistance;
 
-    // Start is called before the first frame update
-    void Start()
+    private GameObject _player;
+    private float _alertTimer;
+    
+    private enum Status
     {
+        Idle,
+        Alert,
+        Shoot,
+        Pull
+    }
+
+    [SerializeField] private Status _status;
+
+    private void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        tongue.position = transform.position;
+    }
+
+    private void Update()
+    {
+        tongueLineRenderer.SetPosition(0, transform.position);
+        tongueLineRenderer.SetPosition(1, tongue.position);
+        
+        switch (_status)
+        {
+            case Status.Idle:
+                // check player below
+                if (transform.position.y - _player.transform.position.y < detectDistance && transform.position.y > _player.transform.position.y)
+                {
+                    // TODO: spawn alert
+                    _status = Status.Alert;
+                }
+                break;
+            case Status.Alert:
+                _alertTimer -= Time.deltaTime;
+                if (_alertTimer < 0)
+                {
+                    // TODO: Destroy alert
+                    _status = Status.Shoot;
+                }
+                break;
+            case Status.Shoot:
+                if (MoveTongue(_player.transform.position))
+                {
+                    // TODO: effect
+                    _status = Status.Pull;
+                }
+                break;
+            case Status.Pull:
+                _player.transform.position = tongue.position;
+                if (MoveTongue(transform.position))
+                {
+                    // TODO: player death
+                    _status = Status.Idle;
+                }
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // return if close enough to dest
+    private bool MoveTongue(Vector2 dest)
     {
-        
+        tongue.position = Vector2.Lerp(tongue.position, dest, .1f);
+        return Vector2.Distance(dest, tongue.position) < .01f;
     }
 }
