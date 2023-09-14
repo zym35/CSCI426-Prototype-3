55ad6a446989a171dd7e7de080ce771fea135580
King Zhou <zhouyimingking@outlook.com>
2023-09-14 10:58:34 -0700

    Modify piranha

--- Assets/Script/Piranha.cs
+++ Assets/Script/Piranha.cs
@@ -8,16 +8,19 @@ public class Piranha : MonoBehaviour
     public LineRenderer tongueLineRenderer;
     public float detectMin, detectMax;
     public float tongueSpeed;
+    public float pullForce;
 
     private GameObject _player;
     private float _alertTimer = 0.5f;
+    private Rigidbody2D _playerRb;
     
     private enum Status
     {
         Idle,
         Alert,
         Shoot,
-        Pull
+        Pull,
+        Retreat
     }
 
     private Status _status;
@@ -26,6 +29,12 @@ public class Piranha : MonoBehaviour
     {
         _player = GameObject.FindGameObjectWithTag("Player");
         tongue.position = transform.position;
+        _playerRb = _player.GetComponent<Rigidbody2D>();
+    }
+
+    public void Cut()
+    {
+        _status = Status.Retreat;
     }
 
     private void Update()
@@ -56,23 +65,26 @@ public class Piranha : MonoBehaviour
             case Status.Shoot:
                 if (TongueIsOut() && BelowPlayer())
                 {
-                    if (MoveTongue(transform.position))
-                    {
-                        tongue.SetParent(transform);
-                        _status = Status.Idle;
-                    }
+                    _status = Status.Retreat;
                 }
                 else if (MoveTongue(_player.transform.position))
                 {
+                    // the tongue captures player
+                    // TODO: call player controller
                     // TODO: effect
                     _status = Status.Pull;
                 }
                 break;
             case Status.Pull:
-                _player.transform.position = tongue.position;
+                tongue.position = _player.transform.position;
+                if (TongueIsOut() && BelowPlayer())
+                {
+                    _status = Status.Retreat;
+                }
+                break;
+            case Status.Retreat:
                 if (MoveTongue(transform.position))
                 {
-                    // TODO: player death
                     tongue.SetParent(transform);
                     _status = Status.Idle;
                 }
@@ -80,8 +92,28 @@ public class Piranha : MonoBehaviour
             default:
                 throw new ArgumentOutOfRangeException();
         }
+    }
 
-        
+    private void FixedUpdate()
+    {
+        if (_status == Status.Pull)
+        {
+            // add force to player
+            Vector2 vec = transform.position - _player.transform.position;
+            float multiplier = 1f / vec.magnitude;
+            _playerRb.AddForce(pullForce * multiplier * vec.normalized);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (_status == Status.Pull && other.CompareTag("Player"))
+        {
+            // TODO: player death
+            tongue.SetParent(transform);
+            tongue.localPosition = Vector3.zero;
+            _status = Status.Idle;
+        }
     }
 
     // return if at dest
