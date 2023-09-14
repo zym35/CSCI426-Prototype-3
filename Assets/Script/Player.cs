4a90c79235a129080c217706e79f58275dcb8598
Ryan Lau <rclau@usc.edu>
2023-09-14 11:09:11 -0700

    player jumping

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rigidbody;
+    public float jumpAmount;
+    Queue<Piranha> attachedPiranhas;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        attachedPiranhas = new Queue<Piranha>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // check for piranhas
+            if (attachedPiranhas.Count > 0)
+            {
+                // detach from the first one
+                Piranha first = attachedPiranhas.Dequeue();
+                first.Cut();
+            }
+            // jump
+            rigidbody.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+        }
+    }
+
+    public void AttachPiranha(Piranha piranha)
+    {
+        attachedPiranhas.Enqueue(piranha);
+    }
+}
