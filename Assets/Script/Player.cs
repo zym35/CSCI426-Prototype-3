8cc77ba52ae86b55b50b581f8116303032fcce6e
Ryan Lau <rclau@usc.edu>
2023-09-14 11:52:48 -0700

    detach piranha function

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,22 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
     public Rigidbody2D rigidbody;
     public float jumpAmount;
+    public float maxSpeed;
+
     Queue<Piranha> attachedPiranhas;
+
+    public TMP_Text scoreCounter;
+    private int score = 0;
+    private float highestPosition;
     
     // Start is called before the first frame update
     void Start()
     {
         attachedPiranhas = new Queue<Piranha>();
+        highestPosition = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (rigidbody.velocity.magnitude > maxSpeed)
+            rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // check for piranhas
@@ -27,12 +38,25 @@ public class Player : MonoBehaviour
                 first.Cut();
                 // jump
                 rigidbody.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+                AddScore(100);
             }
         }
+
     }
 
     public void AttachPiranha(Piranha piranha)
     {
         attachedPiranhas.Enqueue(piranha);
+    }
+
+    public void DetachPiranha()
+    {
+        attachedPiranhas.Dequeue();
+    }
+
+    private void AddScore(int points)
+    {
+        score += points;
+        scoreCounter.text = score.ToString();
     }
 }
